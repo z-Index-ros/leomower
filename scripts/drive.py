e24ca2a3404b46636efd0ae0e01a6e19f966a1fa
Eric Piraux <eric.piraux@stib.brussels>
2022-01-26 22:54:34 +0100

    drive node creation

---
+++ scripts/drive.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+
+class Drive():
+
+    def __init__(self):
+
+        # subscribe to /collision topic
+        rospy.Subscriber('collision', String, self.callback)
+
+        # Create ROS publisher
+        self.publisher = rospy.Publisher("cmd_vel", Twist, queue_size=1)
+
+
+    def callback(self, collision):
+        twist = Twist()
+        if (collision.data == "free"):
+            twist.linear.x = 0.2
+            twist.angular.z = 0
+
+        if (collision.data == "blocked"):
+            twist.linear.x = 0
+            twist.angular.z = 0
+
+        self.publisher.publish(twist)
+        #rospy.loginfo(collision.data)
+
+def main():
+    # init the node
+    rospy.init_node('drive', anonymous=True)
+    Drive()
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    main()
