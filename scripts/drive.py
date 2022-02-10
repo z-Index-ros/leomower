874aad7ef545d85cb4716812951fef207c44c0bf
Eric Piraux <eric.piraux@stib.brussels>
2022-02-10 22:01:12 +0100

    drive fix

--- scripts/drive.py
+++ scripts/drive.py
@@ -8,6 +8,7 @@ class Drive():
 
     def __init__(self):
 
+        rospy.loginfo("Starting driver")
         # subscribe to /collision topic
         rospy.Subscriber('collision', String, self.callback)
 
@@ -18,15 +19,16 @@ class Drive():
     def callback(self, collision):
         twist = Twist()
         if (collision.data == "free"):
-            twist.linear.x = 0.2
+            twist.linear.x = 0.5
             twist.angular.z = 0
 
         if (collision.data == "blocked"):
             twist.linear.x = 0
             twist.angular.z = 1
 
+        rospy.loginfo(twist)
+
         self.publisher.publish(twist)
-        #rospy.loginfo(collision.data)
 
 def main():
     # init the node
