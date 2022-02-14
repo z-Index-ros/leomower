f3d5677daaec4e75219e857a952b8475531ed710
Eric Piraux <eric.piraux@stib.brussels>
2022-02-14 17:03:00 +0100

    drive enhan

--- scripts/drive.py
+++ scripts/drive.py
@@ -18,21 +18,27 @@ class Drive():
         # Create ROS publisher
         self.publisher = rospy.Publisher("cmd_vel", Twist, queue_size=1)
 
+        self.timer = rospy.Timer(rospy.Duration(0.5), self.time_callback)
+
 
 
     def callback(self, collision):
         
         if (collision.data == "free"):
-            self.twist.linear.x = 0.5
+            self.twist.linear.x = 0.2
             self.twist.angular.z = 0
 
         elif (collision.data == "blocked"):
             self.twist.linear.x = 0
-            self.twist.angular.z = 1
+            self.twist.angular.z = 0.2
 
         rospy.loginfo(self.twist)
 
+
+    def time_callback(self, timer):
         self.publisher.publish(self.twist)
+
+
 
 def main():
     # init the node
